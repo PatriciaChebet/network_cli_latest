8cb5a845726d87dd2596dfb99b40dec2174b3f2d
PatriciaChebet <patriciacchebet@gmail.com>
2022-03-13 13:10:57 +0300

    Final changets

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/PatriciaChebet/network_cli_latest/internal/network"
+)
 
 func main() {
 	fmt.Println("Awesome CLI v0.0.1")
+	network.Ping("127.0.0.1")
 }
